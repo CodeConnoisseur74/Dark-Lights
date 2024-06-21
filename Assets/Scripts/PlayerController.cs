37c7b347969d75885e5bdefc59e300ca109397aa
CodeConnoisseur74 <sherry.andrews.com>
2024-06-21 16:56:16 +0200

    add left and right player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    public float Speed;
+
+    bool isFacingLeft;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * Speed * Time.deltaTime, rb.velocity.y);
 
+        if (xInput > 0 && isFacingLeft == true)
+        {
+            isFacingLeft = !isFacingLeft;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            // Flip the player
+        }
+        else if (xInput < 0 && isFacingLeft == false)
+        {
+            // Flip the player
+            isFacingLeft = !isFacingLeft;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
     }
 }
