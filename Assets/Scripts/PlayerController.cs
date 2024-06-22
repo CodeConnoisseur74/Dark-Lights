e838b5da27c693b5d99351160241eb1b2dbe5970
CodeConnoisseur74 <sherry.andrews.com>
2024-06-22 16:50:45 +0200

    add walk, idle and jump animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
     Rigidbody2D rb;
-    public float Speed;
+    public float Speed = 50;
     bool isFacingLeft;
+    Animator anim;
     public float JumpForce;
     public LayerMask WhatIsGround;
     public float JumpRadius;
     bool IsGrounded;
 
     public Transform GroundCheckPos;
+    public int JumpAmount;
+    int JumpCounter;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        IsGrounded = Physics2D.OverlapCircle(GroundCheckPos.position, JumpRadius, WhatIsGround);
+        #region Movement
         float xInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(xInput * Speed * Time.deltaTime, rb.velocity.y);
 
@@ -37,9 +42,45 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+        #endregion
+
+        #region Jump
+        IsGrounded = Physics2D.OverlapCircle(GroundCheckPos.position, JumpRadius, WhatIsGround);
+
+        if (IsGrounded)
+        {
+            JumpCounter = JumpAmount;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && JumpCounter > 0)
         {
             rb.velocity = Vector2.up * JumpForce;
+            if (!IsGrounded)
+            {
+                JumpCounter--;
+            }
+        }
+
+        #endregion
+
+        #region Animations
+
+        if (xInput != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        anim.SetBool("hasJumped", !IsGrounded);
+
+        #endregion
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
@@ -53,4 +94,7 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.DrawSphere(GroundCheckPos.position, JumpRadius);
     }
+
+
+
 }
